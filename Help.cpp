23aea7faffd65b645b6708623fe613e3a2c37f84
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-11 13:04:04 +0300

    Implement user choice handling for help topics

--- Help.cpp
+++ Help.cpp
@@ -11,5 +11,20 @@ int main()
          << "2. Else\n"
          << "Choose One: \n";
     cin >> choice;
-    cout << choice;
+    switch (choice)
+    {
+    case 1:
+        cout << "Using if(statement)\n"
+             << "\telse(statement)\n";
+        break;
+    case 2:
+        cout << "Using switch(expression){\n"
+             << "\tcase constant;\n"
+             << "\tstatement;\n"
+             << "\tbreak;\n"
+             << "\t}";
+        break;
+    default:
+        cout << "Selection not found";
+    }
 }

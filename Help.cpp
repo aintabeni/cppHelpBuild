6ffdeb8ac6548c9192504b6d0284a9179533d8ff
Abenezer Tiglu <aintabeni@gmail.com>
2026-02-11 12:44:19 +0300

    Getting the user's choice

--- Help.cpp
+++ Help.cpp
@@ -2,8 +2,14 @@
 #include <cstdlib>
 using namespace std;
 
-
 int main()
 {
-    
+    // Getting the user choice
+    short choice;
+    cout << "Get help on:\n"
+         << "1. If\n"
+         << "2. Else\n"
+         << "Choose One: \n";
+    cin >> choice;
+    cout << choice;
 }
